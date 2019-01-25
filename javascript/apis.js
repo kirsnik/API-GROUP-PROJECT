dc624198f9af40dbb84ef9e22522455d85e558fb
bphan <btp589@gmail.com>
2019-01-24 20:02:58 -0800

    modified index and apis

--- javascript/apis.js
+++ javascript/apis.js
@@ -1,76 +1,51 @@
-/*HTML from Google's sample code
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Place Searches</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-     /* #map {
-        height: 100%;
+  var queryUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBK8DHqD3RUDa1tWQH0hd4h87dN6d6JmK0&libraries=places"
+
+  var map;
+  var infowindow;
+
+  function initMap() {
+    var pyrmont = {lat: -33.867, lng: 151.195};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: pyrmont,
+      radius: 500,
+      type: ['store']
+    }, callback);
+  }
+
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
       }
-      /* Optional: Makes the sample page fill the window. */
-      /*html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-    <script></script>*/
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">*/
+    }
+  }
 
-      var queryUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBK8DHqD3RUDa1tWQH0hd4h87dN6d6JmK0&libraries=places"
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
 
-      var map;
-      var infowindow;
-
-      function initMap() {
-        var pyrmont = {lat: -33.867, lng: 151.195};
-
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 15
-        });
-
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: pyrmont,
-          radius: 500,
-          type: ['store']
-        }, callback);
-      }
-
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      }
-
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
-   /* </script>
-  </head>
-  <body>
-    <div id="map"></div>
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places&callback=initMap" async defer></script>
-  </body>
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+/* </script>
+</head>
+<body>
+<div id="map"></div>
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places&callback=initMap" async defer></script>
+</body>
 </html>*/
 
 /* START OMDB Database API*/
@@ -78,42 +53,43 @@
 //set an on-click event for the send button 
 //set an on-click for enter button in search
 
-$("#sendbutton").on("click", function() {
-    var selected = $("#search").val();
-    
+$("#sendbutton").on("click", function(event) {
+  event.preventDefault();
+var selected = $("#search").val();
+
 //var selected = "Cinderella"
 
 var queryURL = "https://www.omdbapi.com/?t=" + selected + "&y=&plot=short&apikey=trilogy"
-      
-    // Perfoming an AJAX GET request to our queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
+  
+// Perfoming an AJAX GET request to our queryURL
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
 
-    .then(function(response) {
-      console.log (response);
-        var searchResults = response.Actors;
-        var moviePoster = response.Poster;
-        var genre = response.Genre;
-        var ratings = response.Rated;
-    
-          console.log(searchResults);
-          console.log (moviePoster);
-    
+.then(function(response) {
+  console.log (response);
+    var searchResults = response.Actors;
+    var moviePoster = response.Poster;
+    var genre = response.Genre;
+    var ratings = response.Rated;
 
-    var posterTag = $("<img>");
+      console.log(searchResults);
+      console.log (moviePoster);
 
-    // Giving the image tag an src attribute of a proprty pulled off the
-    // result item
-    posterTag.attr("src", moviePoster);
 
-    // Appending the paragraph and personImage we created to the "gifDiv" div we created
-    $(".imgCorral").html(posterTag);
-    //$(".imgCorral").append(moviePoster);
+var posterTag = $("<img>");
 
-    $("#genre").text (genre);
-    $("#ratings").text (ratings);
+// Giving the image tag an src attribute of a proprty pulled off the
+// result item
+posterTag.attr("src", moviePoster);
+
+// Appending the paragraph and personImage we created to the "gifDiv" div we created
+$(".imgCorral").html(posterTag);
+//$(".imgCorral").append(moviePoster);
+
+$("#genre").text (genre);
+$("#ratings").text (ratings);
 })
 
 })
